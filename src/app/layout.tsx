766b8cc8bbdfa9190017932b080d3ebef74c844d
liutongtong <liutongtong@doba.com>
2025-06-18 09:25:34 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,33 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* header nav with home and about page, fixed top, box shadow, hover with text primary color */}
+        <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-sm shadow-sm">
+          <nav className="flex items-center p-4 justify-between">
+            <div className="flex items-center gap-4">
+              <Link className="hover:text-primary" href="/">
+                Home
+              </Link>
+              <Link className="hover:text-primary" href="/about">
+                About
+              </Link>
+            </div>
+            <div className="flex items-center gap-4">
+              <Button>Login</Button>
+              <Button>Sign up</Button>
+            </div>
+          </nav>
+        </header>
+        {/* flex footer at bottom of page, padding top 68px */}
+        <main className="flex flex-col min-h-screen justify-between pt-[68px]">
+          {children}
+          {/* footer with text center */}
+          <footer className="bg-background/80 backdrop-blur-sm ">
+            <div className="flex items-center p-4 justify-center">
+              <p>© 2025</p>
+            </div>
+          </footer>
+        </main>
       </body>
     </html>
   );
